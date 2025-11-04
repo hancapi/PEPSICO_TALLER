4727d8050f23ab19f589e018d15113e04229a987
hancapi <ha.ancapi@duocuc.cl>
2025-11-04 18:37:17 -0300

    se cambia gitignore y setup.

--- setup.ps1
+++ setup.ps1
@@ -1,22 +1,15 @@
-﻿Write-Host "🚀 Iniciando entorno de desarrollo PepsiCo Taller..."
-
+﻿#🚀 Iniciando entorno de desarrollo PepsiCo Taller...
 # 1. Crear entorno virtual si no existe
-if (-Not (Test-Path "env")) {
-    Write-Host "📦 Creando entorno virtual..."
-    python -m venv env
-}
+python -m venv env
 
 # 2. Activar entorno virtual
-Write-Host "✅ Activando entorno virtual..."
-& .\env\Scripts\Activate.ps1
+.\env\Scripts\Activate
 
 # 3. Instalar dependencias
-Write-Host "📚 Instalando dependencias..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
 # 4. Aplicar migraciones
-Write-Host "🛠️ Aplicando migraciones..."
 python manage.py makemigrations autenticacion
 python manage.py makemigrations vehiculos
 python manage.py makemigrations talleres
@@ -27,5 +20,9 @@ python manage.py makemigrations utils
 python manage.py migrate
 
 # 5. Levantar servidor
-Write-Host "🚦 Levantando servidor en http://127.0.0.1:8000/"
 python manage.py runserver
+
+# 6. crear superusuario
+python manage.py createsuperuser 
+
+
