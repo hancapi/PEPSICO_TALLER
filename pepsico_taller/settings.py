849bed1a8cc5b1567e57fd61e57cfbcd79755a50
hancapi <ha.ancapi@duocuc.cl>
2025-11-20 12:57:33 -0300

    se actualiza setting.py

--- pepsico_taller/settings.py
+++ pepsico_taller/settings.py
@@ -94,7 +94,10 @@ DATABASES = {
         'PORT': config('DB_PORT'),
         'OPTIONS': {
             'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'init_command': (
+                "SET sql_mode='STRICT_TRANS_TABLES', "
+                "NAMES 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci'"
+            ),
         },
     }
 }
